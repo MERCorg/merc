afd104c93ac8a9b3a1c67150cf977f10cc7019f6
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-14 13:15:08 +0100

    Ran formatting, and smaller changes

--- tools/mcrl2/crates/mcrl2/src/atermpp/mod.rs
+++ tools/mcrl2/crates/mcrl2/src/atermpp/mod.rs
@@ -1,11 +1,19 @@
-
-
-mod aterm_string;
+mod aterm;
 mod aterm_list;
+mod aterm_string;
 mod busy_forbidden;
+mod global_aterm_pool;
 mod markable;
+mod random_term;
 mod symbol;
+mod thread_aterm_pool;
 
-pub use symbol::*;
+pub use aterm::*;
+pub use aterm_list::*;
+pub use aterm_string::*;
 pub use busy_forbidden::*;
+pub use global_aterm_pool::*;
 pub use markable::*;
+pub use random_term::*;
+pub use symbol::*;
+pub use thread_aterm_pool::*;
