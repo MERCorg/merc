3e962e15de89a4c8613634ca137e13ec08c1efba
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-14 13:11:44 +0100

    Renamed many functions for consistency

--- tools/mcrl2/crates/mcrl2-sys/src/data.rs
+++ tools/mcrl2/crates/mcrl2-sys/src/data.rs
@@ -23,17 +23,17 @@ pub mod ffi {
         #[cfg(feature = "mcrl2_jittyc")]
         fn mcrl2_create_rewriter_jittyc(data_spec: &data_specification) -> UniquePtr<RewriterCompilingJitty>;
 
-        #[namespace = "atermpp"]
-        type aterm = crate::atermpp::ffi::aterm;
+        #[namespace = "atermpp::detail"]
+        type _aterm = crate::atermpp::ffi::_aterm;
 
         // Recognizers for the various variants of data expressions.
-        fn mcrl2_data_expression_is_variable(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_application(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_abstraction(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_function_symbol(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_where_clause(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_machine_number(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_untyped_identifier(input: &aterm) -> bool;
-        fn mcrl2_data_expression_is_data_expression(input: &aterm) -> bool;
+        fn mcrl2_data_expression_is_variable(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_application(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_abstraction(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_function_symbol(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_where_clause(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_machine_number(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_untyped_identifier(input: &_aterm) -> bool;
+        fn mcrl2_data_expression_is_data_expression(input: &_aterm) -> bool;
     }
 }
