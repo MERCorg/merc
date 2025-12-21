fba8eb80a96f8f83342b41f78dfafe8a83419cca
Maurice Laveaux <m.laveaux@tue.nl>
2025-12-21 15:11:15 +0100

    Created a new crate for the preorder algorithms

---
+++ crates/preorder/src/antichain.rs
@@ -0,0 +1,35 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+
+use merc_utilities::VecSet;
+
+/// An antichain is a data structure that stores pairs of (s, T) \subset S x 2^S, where `S` is a set of elements that have a total order <.
+/// The antichain maintains the invariant that for any two pairs (s1, T1) and (s2, T2) in the antichain, neither s1 < s2 nor s2 < s1 holds, i.e.,
+/// it is dual to a chain.
+pub struct Antichain<K, V> {
+    storage: HashMap<K, VecSet<VecSet<V>>>,
+
+    /// The largest size of the antichain.
+    max_antichain: usize,
+    /// Number of times a pair was inserted into the antichain.
+    antichain_misses: usize, 
+    /// Number of times antichain_insert was called.
+    antichain_inserts: usize,     
+}
+
+impl<K: Eq + Hash, V: Ord> Antichain<K, V> {
+
+    /// Inserts the given (s, T) pair into the antichain and returns true iff it was
+    /// not already present.
+    pub fn insert(&mut self, key: K, value: VecSet<V>) -> bool {
+        self.storage.entry(key)
+            .or_insert(VecSet::singleton(value));
+
+        true
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+}
---
+++ crates/preorder/src/preorder.rs
@@ -0,0 +1,27 @@
+use clap::ValueEnum;
+use merc_lts::LTS;
+use merc_utilities::Timing;
+
+use crate::{ExplorationStrategy, failures_refinement, is_failures_refinement};
+
+#[derive(Clone, Debug, ValueEnum)]
+pub enum RefinementType {
+    Trace,
+
+}
+
+
+pub fn is_refinement<L: LTS>(impl_lts: L, spec_lts: L, preorder: RefinementType, timing: &mut Timing) -> bool {
+    match preorder {
+        RefinementType::Trace => {
+            is_failures_refinement::<L, false>(
+                impl_lts,
+                spec_lts,
+                RefinementType::Trace,
+                ExplorationStrategy::BFS,
+                true,
+                timing,
+            )
+        }
+    } 
+}
