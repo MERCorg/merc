b77243ce8730b344d9bc5f47feb890b4cd1cc60c
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-02 22:25:29 +0100

    Added both formats.

---
+++ crates/symbolic/src/io.rs
@@ -0,0 +1,25 @@
+use std::{ffi::OsStr, path::Path};
+
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+#[cfg_attr(feature = "cadp", derive(clap::ValueEnum))]
+pub enum SymFormat {
+    ///
+    Sym,
+    Sylvan
+}
+
+
+/// Guesses the symbolic LTS file format from the file extension. Returns None if it cannot be determined.
+pub fn guess_format_from_extension(path: &Path, format: Option<SymFormat>) -> Option<SymFormat> {
+    if let Some(format) = format {
+        return Some(format);
+    }
+    
+    if path.extension() == Some(OsStr::new("aut")) {
+        Some(SymFormat::Sylvan)
+    } else if path.extension() == Some(OsStr::new("sym")) {
+        Some(SymFormat::Sym)
+    } else {
+        None
+    }
+}
