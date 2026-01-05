83c125b9aeb03175de5169b61bdc0cdef4984630
Maurice Laveaux <m.laveaux@tue.nl>
2026-01-05 19:57:48 +0100

    Moved the Repeat helper to a different module

--- crates/vpg/src/variability_zielonka.rs
+++ crates/vpg/src/variability_zielonka.rs
@@ -34,29 +34,8 @@ use crate::Priority;
 use crate::VariabilityParityGame;
 use crate::VariabilityPredecessors;
 use crate::VertexIndex;
-use crate::PG;
-
-/// Utility to print a repeated static string a given number of times.
-pub struct Repeat {
-    s: &'static str,
-    times: usize,
-}
-
-impl Repeat {
-    /// Creates a new Repeat instance.
-    pub fn new(s: &'static str, times: usize) -> Self {
-        Self { s, times }
-    }
-}
-
-impl fmt::Display for Repeat {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        for _ in 0..self.times {
-            f.write_str(self.s)?;
-        }
-        Ok(())
-    }
-}
+use crate::solve_zielonka;
+use crate::x_and_not_x;
 
 /// Variant of the Zielonka algorithm to use.
 #[derive(ValueEnum, Debug, Clone, Copy, PartialEq, Eq)]
